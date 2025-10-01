fe05e19429054ae25e74542dd9eb3af400733d26
Gabriela\Gab-laptop <rafal.jan.laskowski@gmail.com>
2025-10-01 20:11:48 +0200

    Oh boy here we go - Changed display so it awlays displays 0 if user didn't type anything nor the answer isn't displayed - Cannot type multiples 0 if the first number is 0 - Cannot type additonal commas if there is already one - Trying to divide by zero will display Error - "Equal" button won't work unless user chose operetor - "Clear" button will now clear everything. User will now have to start typing all over again
    To do
    - Make more advanced clear button
    - Name controlers and fxml files appropriately
    - Show Cleary that operetor was used to prevent user from typing additional numbers before pressing "equal" button
    - Make backspace button(?)
    - Add option to type via keyboard(?)
    - Explein code to Saroj

--- src/main/java/dk/easv/advancedcalculator/HelloController.java
+++ src/main/java/dk/easv/advancedcalculator/HelloController.java
@@ -2,119 +2,198 @@ package dk.easv.advancedcalculator;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class HelloController {
 
     @FXML
     private TextField txtCalculate;
-    private boolean state = true;
+    private boolean state;
     private double input1;
     private double input2;
     private String operator;
 
 
     public void btn0(ActionEvent actionEvent) {
-        txtCalculate.setText(txtCalculate.getText()+"0");
+        if (!txtCalculate.getText().startsWith("0"))
+            txtCalculate.setText(txtCalculate.getText()+"0");
     }
 
     public void btn1(ActionEvent actionEvent) {
-        txtCalculate.setText(txtCalculate.getText()+"1");
+        if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
+            txtCalculate.setText("1");
+        else
+            txtCalculate.setText(txtCalculate.getText()+"1");
     }
 
     public void btn2(ActionEvent actionEvent) {
-        txtCalculate.setText(txtCalculate.getText()+"2");
+        if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
+            txtCalculate.setText("2");
+        else
+            txtCalculate.setText(txtCalculate.getText()+"2");
     }
 
     public void btn3(ActionEvent actionEvent) {
-        txtCalculate.setText(txtCalculate.getText()+"3");
+        if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
+            txtCalculate.setText("3");
+        else
+            txtCalculate.setText(txtCalculate.getText()+"3");
     }
 
     public void btn4(ActionEvent actionEvent) {
-        txtCalculate.setText(txtCalculate.getText()+"4");
+        if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
+            txtCalculate.setText("4");
+        else
+            txtCalculate.setText(txtCalculate.getText()+"4");
     }
 
     public void btn5(ActionEvent actionEvent) {
-        txtCalculate.setText(txtCalculate.getText()+"5");
+        if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
+            txtCalculate.setText("5");
+        else
+            txtCalculate.setText(txtCalculate.getText()+"5");
     }
 
     public void btn6(ActionEvent actionEvent) {
-        txtCalculate.setText(txtCalculate.getText()+"6");
+        if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
+            txtCalculate.setText("6");
+        else
+            txtCalculate.setText(txtCalculate.getText()+"6");
     }
 
     public void btn7(ActionEvent actionEvent) {
-        txtCalculate.setText(txtCalculate.getText()+"7");
+        if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
+            txtCalculate.setText("7");
+        else
+            txtCalculate.setText(txtCalculate.getText()+"7");
     }
 
     public void btn8(ActionEvent actionEvent) {
-        txtCalculate.setText(txtCalculate.getText()+"8");
+        if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
+            txtCalculate.setText("8");
+        else
+            txtCalculate.setText(txtCalculate.getText()+"8");
     }
 
     public void btn9(ActionEvent actionEvent) {
-        txtCalculate.setText(txtCalculate.getText()+"9");
+        if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
+            txtCalculate.setText("9");
+        else
+            txtCalculate.setText(txtCalculate.getText()+"9");
     }
 
     public void btnPl(ActionEvent actionEvent) {
-        input1 = Double.parseDouble(txtCalculate.getText());
-        operator = "+";
-        txtCalculate.setText("");
+        if (input1 == 0) {
+            input1 = Double.parseDouble(txtCalculate.getText());
+            operator = "+";
+            txtCalculate.setText("0");
+            state = true;
+        }
+        else {
+            operator = "+";
+        }
     }
 
     public void btnMi(ActionEvent actionEvent) {
-        input1 = Double.parseDouble(txtCalculate.getText());
-        operator = "-";
-        txtCalculate.setText("");
+        if (input1 == 0) {
+            input1 = Double.parseDouble(txtCalculate.getText());
+            operator = "-";
+            txtCalculate.setText("0");
+            state = true;
+        }
+        else {
+            operator = "-";
+        }
     }
 
     public void btnMu(ActionEvent actionEvent) {
-        input1 = Double.parseDouble(txtCalculate.getText());
-        operator = "*";
-        txtCalculate.setText("");
+        if (input1 == 0) {
+            input1 = Double.parseDouble(txtCalculate.getText());
+            operator = "*";
+            txtCalculate.setText("0");
+            state = true;
+        }
+        else {
+            operator = "*";
+        }
     }
 
     public void btnDi(ActionEvent actionEvent) {
-        input1 = Double.parseDouble(txtCalculate.getText());
-        operator = "/";
-        txtCalculate.setText("");
+        if (input1 == 0) {
+            input1 = Double.parseDouble(txtCalculate.getText());
+            operator = "/";
+            txtCalculate.setText("0");
+            state = true;
+        }
+        else {
+            operator = "/";
+        }
     }
 
     public void btnPr(ActionEvent actionEvent) {
-        input1 = Double.parseDouble(txtCalculate.getText());
-        operator = "%";
-        txtCalculate.setText("");
+        if (input1 == 0) {
+            input1 = Double.parseDouble(txtCalculate.getText());
+            operator = "%";
+            txtCalculate.setText("0");
+            state = true;
+        }
+        else {
+            operator = "%";
+        }
     }
 
     public void btnCo(ActionEvent actionEvent) {
-        txtCalculate.setText(txtCalculate.getText()+".");
-
+        if (!txtCalculate.getText().contains("."))  //Checks if there is already comma
+            txtCalculate.setText(txtCalculate.getText()+".");
     }
 
-    public void btnClear(ActionEvent actionEvent) {
-        txtCalculate.setText("");
+    public void btnClear(ActionEvent actionEvent) { //Button now clears the textfield and anything left in the memory
+        txtCalculate.setText("0");
+        input1 = 0;
+        input2 = 0;
+        operator = "";
+        state = false;
     }
 
     public void btnPM(ActionEvent actionEvent) {
-    }
-
-    public void btnEq(ActionEvent actionEvent) {
-        input2 = Double.parseDouble(txtCalculate.getText());
-        txtCalculate.setText("");
-        switch (operator) {
-            case "+":
-                txtCalculate.setText(String.valueOf(input1+input2));
-                break;
-            case "-":
-                txtCalculate.setText(String.valueOf(input1-input2));
-                break;
-            case "*":
-                txtCalculate.setText(String.valueOf(input1*input2));
-                break;
-            case "/":
-                txtCalculate.setText(String.valueOf(input1/input2));
-                break;
-            case "%":
-                txtCalculate.setText(String.valueOf(input1%input2));
+        if (!txtCalculate.getText().contains("-")){
+            txtCalculate.setText("-"+txtCalculate.getText());
         }
+        else {
+            txtCalculate.setText(txtCalculate.getText().substring(1));
+        }
+
+    }
+    public void btnEq(ActionEvent actionEvent) {
+        if (state) {
+            input2 = Double.parseDouble(txtCalculate.getText());
+            txtCalculate.setText("");
+            switch (operator) {
+                case "+":
+                    txtCalculate.setText(String.valueOf(input1 + input2));
+                    break;
+                case "-":
+                    txtCalculate.setText(String.valueOf(input1 - input2));
+                    break;
+                case "*":
+                    txtCalculate.setText(String.valueOf(input1 * input2));
+                    break;
+                case "/":
+                    if (input2 == 0) {
+                        txtCalculate.setText("Cannot divide by zero");
+                    }
+                    else {
+                        txtCalculate.setText(String.valueOf(input1 / input2));
+                    }
+                    break;
+                case "%":
+                    txtCalculate.setText(String.valueOf(input1 % input2));
+                    break;
+            }
+        }
+        input1 = 0;
+        input2 = 0;
+        operator = "";
+        state = false;
     }
 }
