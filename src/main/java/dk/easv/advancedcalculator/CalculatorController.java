98e6ff0341a34bdf34c7db3571aa8cb0bd1b35d0
Gabriela\Gab-laptop <rafal.jan.laskowski@gmail.com>
2025-10-03 14:26:12 +0200

    Changes - Controlers and Fxml files are named appropriately
    In progress:
    - Highlighting operetors
    
    To do:
    - Make more advanced clear button
    - Make backspace button(?)
    - Add option to type via keyboard(?)

--- src/main/java/dk/easv/advancedcalculator/CalculatorController.java
+++ src/main/java/dk/easv/advancedcalculator/CalculatorController.java
@@ -1,10 +1,19 @@
 package dk.easv.advancedcalculator;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class HelloController {
+public class CalculatorController {
+
+    @FXML
+    private Button Pl;
+    @FXML
+    private Button Mi;
+    @FXML
+    private Button Mu;
+    @FXML
+    private Button Di;
 
     @FXML
     private TextField txtCalculate;
@@ -14,76 +23,87 @@ public class HelloController {
     private double result;
     private String operator;
 
-
-    public void btn0(ActionEvent actionEvent) {
+    public void btn0() {
         if (!txtCalculate.getText().startsWith("0"))
             txtCalculate.setText(txtCalculate.getText()+"0");
     }
 
-    public void btn1(ActionEvent actionEvent) {
+    public void btn1() {
         if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
             txtCalculate.setText("1");
         else
             txtCalculate.setText(txtCalculate.getText()+"1");
     }
 
-    public void btn2(ActionEvent actionEvent) {
+    public void btn2() {
         if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
             txtCalculate.setText("2");
         else
             txtCalculate.setText(txtCalculate.getText()+"2");
     }
 
-    public void btn3(ActionEvent actionEvent) {
+    public void btn3() {
         if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
             txtCalculate.setText("3");
         else
             txtCalculate.setText(txtCalculate.getText()+"3");
     }
 
-    public void btn4(ActionEvent actionEvent) {
+    public void btn4() {
         if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
             txtCalculate.setText("4");
         else
             txtCalculate.setText(txtCalculate.getText()+"4");
     }
 
-    public void btn5(ActionEvent actionEvent) {
+    public void btn5() {
         if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
             txtCalculate.setText("5");
         else
             txtCalculate.setText(txtCalculate.getText()+"5");
     }
 
-    public void btn6(ActionEvent actionEvent) {
+    public void btn6() {
         if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
             txtCalculate.setText("6");
         else
             txtCalculate.setText(txtCalculate.getText()+"6");
     }
 
-    public void btn7(ActionEvent actionEvent) {
+    public void btn7() {
         if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
             txtCalculate.setText("7");
         else
             txtCalculate.setText(txtCalculate.getText()+"7");
     }
 
-    public void btn8(ActionEvent actionEvent) {
+    public void btn8() {
         if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
             txtCalculate.setText("8");
         else
             txtCalculate.setText(txtCalculate.getText()+"8");
     }
 
-    public void btn9(ActionEvent actionEvent) {
+    public void btn9() {
         if (txtCalculate.getLength()==1 && txtCalculate.getText().startsWith("0"))
             txtCalculate.setText("9");
         else
             txtCalculate.setText(txtCalculate.getText()+"9");
     }
 
-    public void btnPl(ActionEvent actionEvent) {
+    public void ClearOperatorHighlights() {
+        Pl.setStyle("-fx-background-color: #E4983C; fx-text-fill: black;");
+        Mi.setStyle("-fx-background-color: #E4983C; fx-text-fill: black;");
+        Mu.setStyle("-fx-background-color: #E4983C; fx-text-fill: black;");
+        Di.setStyle("-fx-background-color: #E4983C; fx-text-fill: black;");
+    }
+
+    public void higllightOperator(Button button) {
+        ClearOperatorHighlights();
+        button.setStyle("-fx-background-color: black; -fx-text-fill: #E4983C;");
+    }
+
+    public void btnPl() {
         if (input1 == 0) {
             input1 = Double.parseDouble(txtCalculate.getText());
             operator = "+";
@@ -93,9 +113,10 @@ public class HelloController {
         else {
             operator = "+";
         }
+        higllightOperator(Pl);
     }
 
-    public void btnMi(ActionEvent actionEvent) {
+    public void btnMi() {
         if (input1 == 0) {
             input1 = Double.parseDouble(txtCalculate.getText());
             operator = "-";
@@ -105,9 +126,10 @@ public class HelloController {
         else {
             operator = "-";
         }
+        higllightOperator(Mi);
     }
 
-    public void btnMu(ActionEvent actionEvent) {
+    public void btnMu() {
         if (input1 == 0) {
             input1 = Double.parseDouble(txtCalculate.getText());
             operator = "*";
@@ -117,9 +139,10 @@ public class HelloController {
         else {
             operator = "*";
         }
+        higllightOperator(Mu);
     }
 
-    public void btnDi(ActionEvent actionEvent) {
+    public void btnDi() {
         if (input1 == 0) {
             input1 = Double.parseDouble(txtCalculate.getText());
             operator = "/";
@@ -129,9 +152,10 @@ public class HelloController {
         else {
             operator = "/";
         }
+        higllightOperator(Di);
     }
 
-    public void btnPr(ActionEvent actionEvent) {
+    public void btnPr() {
         if (input1 == 0) {
             input1 = Double.parseDouble(txtCalculate.getText());
             operator = "%";
@@ -143,20 +167,22 @@ public class HelloController {
         }
     }
 
-    public void btnCo(ActionEvent actionEvent) {
+    public void btnCo() {
         if (!txtCalculate.getText().contains("."))  //Checks if there is already comma
             txtCalculate.setText(txtCalculate.getText()+".");
     }
 
-    public void btnClear(ActionEvent actionEvent) { //Button now clears the textfield and anything left in the memory
+    public void btnClear() { //Button now clears the text field and anything left in the memory
         txtCalculate.setText("0");
         input1 = 0;
         input2 = 0;
+        result = 0;
         operator = "";
         state = false;
+        ClearOperatorHighlights();
     }
 
-    public void btnPM(ActionEvent actionEvent) {
+    public void btnPM() {
         if (!txtCalculate.getText().contains("-")){
             txtCalculate.setText("-"+txtCalculate.getText());
         }
@@ -165,7 +191,7 @@ public class HelloController {
         }
 
     }
-    public void btnEq(ActionEvent actionEvent) {
+    public void btnEq() {
         if (state) {
             input2 = Double.parseDouble(txtCalculate.getText());
             txtCalculate.setText("");
@@ -207,5 +233,6 @@ public class HelloController {
         result = 0;
         operator = "";
         state = false;
+        ClearOperatorHighlights();
     }
 }
